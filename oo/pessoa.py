f839633773ee8fdd9a1275f5a9be69cafff7dd59
BarthJr <juniior.barth@gmail.com>
2019-02-08 12:07:06 -0200

    Created name and age instance attributes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=25):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Hello {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Barth')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
